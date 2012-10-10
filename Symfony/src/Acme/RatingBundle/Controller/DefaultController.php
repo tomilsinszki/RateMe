5b7c1c39a997f84beaebd18a79a1b747ce83cb79
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2012-10-10 06:59:50 -0700

    Creating homepage.

--- Symfony/src/Acme/RatingBundle/Controller/DefaultController.php
+++ Symfony/src/Acme/RatingBundle/Controller/DefaultController.php
@@ -6,8 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AcmeRatingBundle:Default:index.html.twig', array('name' => $name));
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('alphanumeric_value', 'text')
+            ->getForm();
+
+        return $this->render('AcmeRatingBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
