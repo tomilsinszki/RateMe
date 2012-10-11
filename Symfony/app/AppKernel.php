e976cdf3b360b936a1c76dee7f6e0dc97daf0ed0
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2012-10-11 03:07:39 -0700

    Adding fixtures.

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Acme\UserBundle\AcmeUserBundle(),
             new Acme\RatingBundle\AcmeRatingBundle(),
         );
