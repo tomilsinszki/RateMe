1db1bd481440dcf7119bdaa1c583b42cfab43fec
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2012-10-11 05:23:32 -0700

    Moving Identifier related classes and twig files to their appropriate places.

--- Symfony/src/Acme/RatingBundle/Controller/DefaultController.php
+++ Symfony/src/Acme/RatingBundle/Controller/DefaultController.php
@@ -8,13 +8,5 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $defaultData = array();
-        $form = $this->createFormBuilder($defaultData)
-            ->add('alphanumeric_value', 'text')
-            ->getForm();
-
-        return $this->render('AcmeRatingBundle:Default:index.html.twig', array(
-            'form' => $form->createView(),
-        ));
     }
 }
---
+++ Symfony/src/Acme/RatingBundle/Controller/IdentifierController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Acme\RatingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IdentifierController extends Controller
+{
+    public function indexAction()
+    {
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('alphanumericValue', 'text')
+            ->getForm();
+
+        return $this->render('AcmeRatingBundle:Identifier:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function searchAction(Request $request)
+    {
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('alphanumericValue', 'text')
+            ->getForm();
+        
+        if ( $request->isMethod('POST') ) {
+            $form->bind($request);
+
+            if ( $form->isValid() ) {
+                // TODO: check if alphanumeric value exists, if not error page
+
+                // TODO: get alphanumeric value
+                $alphanumericValue = $form->get('alphanumericValue')->getData();
+
+                $identifier = $this->getDoctrine()
+                    ->getRepository('AcmeRatingBundle:Identifier')
+                    ->findOneByAlphanumericValue($alphanumericValue);
+
+                if ( empty($identifier) === TRUE ) {
+                    return $this->redirect($this->generateUrl('identifier_not_found'));
+                }
+                /*
+                else {
+                }
+                */
+
+                //return $this->redirect($this->generateUrl('acme_user_registration'));
+            }
+        }
+    }
+
+    public function notExistsAction()
+    {
+        return new Response('<html><body>Nincs ilyen kód.</body></html>');
+    }
+}
