aa69fc2703b64ac1af12fd40ed78efa1436f0e49
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2013-04-17 19:25:18 +0200

    Implemented dummy email sending.

--- Symfony/src/Acme/RatingBundle/Command/EmailRatingRequestForContactsCommand.php
+++ Symfony/src/Acme/RatingBundle/Command/EmailRatingRequestForContactsCommand.php
@@ -12,15 +12,27 @@ class EmailRatingRequestForContactsCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this
-            ->setName('email:rating_request_for_contacts')
-            ->setDescription('Sending rating requests for contacts')
-        ;
+        $this->setName('email:rating_request_for_contacts');
+        $this->setDescription('Sending rating requests for contacts');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello');
+        $message = \Swift_Message::newInstance();
+        $message->setSubject('Hello Email');
+        $message->setFrom(array('vidanet@rate.me.uk' => 'Vidanet'));
+        $message->setTo('ilsinszkitamas@gmail.com');
+        $message->setBody('mizu?');
+
+        $container = $this->getContainer();
+        $mailer = $container->get('mailer');
+
+        $mailer->send($message);
+
+        $spool = $mailer->getTransport()->getSpool();
+        $transport = $container->get('swiftmailer.transport.real');
+
+        $spool->flushQueue($transport);
     }
 }
 
