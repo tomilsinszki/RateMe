9ec226c38b050e511d2bfb4ceff2b3d0f2beffbe
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2014-01-13 16:18:29 +0100

    Change identifiers.

---
+++ Symfony/sql/2014-01-13-regenerateAlphanumericAndQRCodes.sql
@@ -0,0 +1,22 @@
+UPDATE identifier SET alphanumeric_value='RSDG', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FRSDG&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Somtel');
+
+UPDATE identifier SET alphanumeric_value='QDGW', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FQDGW&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Győr (Teleki u.)');
+
+UPDATE identifier SET alphanumeric_value='YSSQ', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FYSSQ&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Kaposvár (Honvéd u.)');
+
+UPDATE identifier SET alphanumeric_value='GEVS', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FGEVS&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Mosonmmagyaróvár (TESCO)');
+
+UPDATE identifier SET alphanumeric_value='TSGF', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FTSGF&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Csorna');
+
+UPDATE identifier SET alphanumeric_value='HSDT', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FHSDT&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Győr Pláza');
+
+UPDATE identifier SET alphanumeric_value='LKNC', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FLKNC&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Kaposvár (Szántó u.)');
+
+UPDATE identifier SET alphanumeric_value='SVXE', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FSVXE&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Kapuvár');
+
+UPDATE identifier SET alphanumeric_value='GTGV', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FGTGV&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Mosonmmagyaróvár');
+
+UPDATE identifier SET alphanumeric_value='ZJWD', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FZJWD&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Pécs-Harkány');
+
+UPDATE identifier SET alphanumeric_value='POER', qr_code_url='http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fhely%2Fazonosito%2FPOER&size=250x250' WHERE id=(SELECT identifier_id FROM rateable_collection WHERE name='Tata');
+
