58d969cebdcc7a7bbee488679cf311f531c7df30
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2014-03-01 17:43:00 +0100

    Intro to Vidanet.

---
+++ Symfony/sql/2014-03-01-2-addIdentifierForRateables.sql
@@ -0,0 +1,135 @@
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FIGDE&size=250x250', 'IGDE', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='IGDE') WHERE name='Baranyainé Herold Andrea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FTSGS&size=250x250', 'TSGS', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='TSGS') WHERE name='Varga Andrea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHRSV&size=250x250', 'HRSV', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HRSV') WHERE name='Juhász Katalin';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FGFES&size=250x250', 'GFES', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='GFES') WHERE name='Menyhárt János';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHRBY&size=250x250', 'HRBY', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HRBY') WHERE name='Vrábel Mihály';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FZXVL&size=250x250', 'ZXVL', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='ZXVL') WHERE name='Madár Zsanett';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FURVZ&size=250x250', 'URVZ', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='URVZ') WHERE name='Bokor Zsuzsanna';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FJLSR&size=250x250', 'JLSR', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='JLSR') WHERE name='Ihász Bea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FREGT&size=250x250', 'REGT', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='REGT') WHERE name='Némethné Wachtler Ágota';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FYZAB&size=250x250', 'YZAB', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='YZAB') WHERE name='Szőke Veronika';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FORGS&size=250x250', 'ORGS', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='ORGS') WHERE name='Varga Ibolya';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FPLCG&size=250x250', 'PLCG', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='PLCG') WHERE name='Adamecz Éva';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FVHSI&size=250x250', 'VHSI', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='VHSI') WHERE name='Horváth Rebeka';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FBSRO&size=250x250', 'BSRO', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='BSRO') WHERE name='Fisli Tiborné';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FAHXF&size=250x250', 'AHXF', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='AHXF') WHERE name='Szatmári Dóra';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHLTG&size=250x250', 'HLTG', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HLTG') WHERE name='Varga Andrea Lusy';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FUBXT&size=250x250', 'UBXT', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='UBXT') WHERE name='Bécsi Nikolett';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FKOBH&size=250x250', 'KOBH', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='KOBH') WHERE name='Végh Zsuzsanna';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FZTPU&size=250x250', 'ZTPU', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='ZTPU') WHERE name='Fördős Andrea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FAGFP&size=250x250', 'AGFP', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='AGFP') WHERE name='Hende Judit';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FVOZI&size=250x250', 'VOZI', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='VOZI') WHERE name='Freyné Anka Zsuzsanna';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHOBD&size=250x250', 'HOBD', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HOBD') WHERE name='Némedi Rita';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FPDGH&size=250x250', 'PDGH', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='PDGH') WHERE name='Major Edith';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FVAGN&size=250x250', 'VAGN', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='VAGN') WHERE name='Eizler Kitti';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FNFXT&size=250x250', 'NFXT', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='NFXT') WHERE name='Szénásy Zoltánné';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FBPEG&size=250x250', 'BPEG', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='BPEG') WHERE name='Barkó Renáta';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FUMIO&size=250x250', 'UMIO', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='UMIO') WHERE name='Járfás Nóra';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FBBER&size=250x250', 'BBER', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='BBER') WHERE name='Kotfás Judit';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FGRAD&size=250x250', 'GRAD', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='GRAD') WHERE name='Kovács Márta';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHINR&size=250x250', 'HINR', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HINR') WHERE name='Kutasi Krisztina';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FGOHD&size=250x250', 'GOHD', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='GOHD') WHERE name='Lapat Krisztina';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FTPAD&size=250x250', 'TPAD', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='TPAD') WHERE name='Megla Dóra';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FVGJH&size=250x250', 'VGJH', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='VGJH') WHERE name='Mócsán Dea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FURTA&size=250x250', 'URTA', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='URTA') WHERE name='Molnár Tímea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FCDSU&size=250x250', 'CDSU', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='CDSU') WHERE name='Nagyné Balogh Tünde';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FBOPF&size=250x250', 'BOPF', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='BOPF') WHERE name='Némethné Kámán Mária';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FHBGH&size=250x250', 'HBGH', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='HBGH') WHERE name='Révészné Borsos Anita';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FJISK&size=250x250', 'JISK', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='JISK') WHERE name='Siposné Deák Tímea';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FLOSM&size=250x250', 'LOSM', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='LOSM') WHERE name='Szűcs Ágnes';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FYSMI&size=250x250', 'YSMI', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='YSMI') WHERE name='Árvainé Kovács Anikó';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FNOVS&size=250x250', 'NOVS', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='NOVS') WHERE name='Németh Nikolett';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FKJGA&size=250x250', 'KJGA', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='KJGA') WHERE name='Nyiri Ágnes';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FGTKE&size=250x250', 'GTKE', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='GTKE') WHERE name='Petroviczné Szalai Veronika';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FTRIG&size=250x250', 'TRIG', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='TRIG') WHERE name='Fehérvári Ildikó';
+
+INSERT INTO identifier (qr_code_url, alphanumeric_value, created, updated) VALUE ('http://api.qrserver.com/v1/create-qr-code/?data=http%3A%2F%2Frate.me.uk%2Fazonosito%2FKREX&size=250x250', 'KREX', NOW(), NOW());
+UPDATE rateable SET identifier_id=(SELECT id FROM identifier WHERE alphanumeric_value='KREX') WHERE name='Hegyes Tímea';
+
