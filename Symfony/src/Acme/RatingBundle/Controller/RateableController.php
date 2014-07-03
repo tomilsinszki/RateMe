933982df7dc34b18739bc3fa3db0af5b322665b7
András Rutkai <riskawarrior@live.com>
2014-07-03 14:34:34 +0200

    Added redirect for rateable edit page (after editing).

--- Symfony/src/Acme/RatingBundle/Controller/RateableController.php
+++ Symfony/src/Acme/RatingBundle/Controller/RateableController.php
@@ -72,6 +72,10 @@ class RateableController extends Controller
                     
                     $this->getDoctrine()->getManager()->flush();
                 }
+
+                return $this->redirect($this->generateUrl('rateable_collection_profile_edit_by_id', array(
+                    'id' => $rateable->getCollection()->getId(),
+                )));
             }
         }
         
