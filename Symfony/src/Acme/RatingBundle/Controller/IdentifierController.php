42d455c800accf9ef34678f9b75d7eb7b7888a0e
Bozsik József <bozsikj@gmail.com>
2014-07-20 11:32:02 +0200

    Mainpage error message refact.

--- Symfony/src/Acme/RatingBundle/Controller/IdentifierController.php
+++ Symfony/src/Acme/RatingBundle/Controller/IdentifierController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Acme\RatingBundle\Utility\Validator;
+use Symfony\Component\Form\FormError;
 
 class IdentifierController extends Controller
 {
@@ -16,7 +17,7 @@ class IdentifierController extends Controller
             ->add('alphanumericValue', 'text', array('attr' => array('placeholder' => $this->get('translator')->trans('Enter Code Here', array(), 'identifier'), 'autocomplete' => 'off')))
             ->getForm();
         
-        $signUpForm = $this->createFormBuilder($defaultData)
+        $signUpForm = $this->createFormBuilder($defaultData, array('csrf_protection' => false))
             ->add('lastName', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('firstName', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('email', 'text', array('attr' => array('autocomplete' => 'off')))
@@ -37,54 +38,84 @@ class IdentifierController extends Controller
             ->add('alphanumericValue', 'text', array('attr' => array('placeholder' => 'Add meg a 4 jegyű kódot', 'autocomplete' => 'off')))
             ->getForm();
         
-        if ( $request->isMethod('POST') ) {
-            $form->bind($request);
-
-            if ( $form->isValid() ) {
-                $alphanumericValue = $form->get('alphanumericValue')->getData();
-                if ( empty($alphanumericValue) === TRUE ) {
-                    $this->get('session')->setFlash('notice', 'Nem adtál meg kódot');
-                    return $this->redirect($this->generateUrl('_welcome'));
-                }
-
-                $identifier = $this->getDoctrine()->getRepository('AcmeRatingBundle:Identifier')->findOneByAlphanumericValue($alphanumericValue);
-                if ( empty($identifier) === TRUE ) {
-                    $this->get('session')->setFlash('notice', 'Az általad megadott kód nem létezik');
-                    return $this->redirect($this->generateUrl('_welcome'));
-                }
-                
-                $rateable = $this->getDoctrine()->getRepository('AcmeRatingBundle:Rateable')->findOneByIdentifier($identifier);
-                if ( empty($rateable) === FALSE )
-                    return $this->redirect($this->generateUrl('rateable_main', array('alphanumericValue' => $alphanumericValue)));
-                
-                $collection = $this->getDoctrine()->getRepository('AcmeRatingBundle:RateableCollection')->findOneByIdentifier($identifier);
-                if ( empty($collection) === FALSE )
-                    return $this->redirect($this->generateUrl('rateable_collection_main', array('alphanumericValue' => $alphanumericValue)));
-                
-                $this->get('session')->setFlash('notice', 'Az általad megadott kód nem létezik');
-                return $this->redirect($this->generateUrl('_welcome'));
-            }
-        }
-    }
-    
-    public function signUpAction(Request $request) {
-        $defaultData = array();
-        $signUpForm = $this->createFormBuilder($defaultData)
+        $signUpForm = $this->createFormBuilder($defaultData, array('csrf_protection' => false))
             ->add('lastName', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('firstName', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('email', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('company', 'text', array('attr' => array('autocomplete' => 'off')))
             ->add('message', 'textarea', array('attr' => array('autocomplete' => 'off')))
             ->getForm();
+        
         if ( $request->isMethod('POST') ) {
-            $signUpForm->bind($request);
-            $email = $signUpForm->get('email')->getData();
-            if(FALSE === Validator::isEmailAddressValid($email)) {
-                $this->get('session')->setFlash('notice', 'A megadott email cím nem megfelelő!');
-                return $this->redirect($this->generateUrl('_welcome'));
+            $form->bind($request);
+
+            if ( $form->isValid() ) {
+                $alphanumericValue = $form->get('alphanumericValue')->getData();
+                if ( empty($alphanumericValue) === TRUE ) {
+                    $form->addError(new FormError('Nem adtál meg kódot'));                    
+                    return $this->render('AcmeRatingBundle:Identifier:index.html.twig', array(
+                        'form'       => $form->createView(),
+                        'signUpForm' => $signUpForm->createView(),
+                    ));  
+                }
+
+                $identifier = $this->getDoctrine()->getRepository('AcmeRatingBundle:Identifier')->findOneByAlphanumericValue($alphanumericValue);
+                if ( empty($identifier) === TRUE ) {
+                    $form->addError(new FormError('Az általad megadott kód nem létezik'));                      
+                    return $this->render('AcmeRatingBundle:Identifier:index.html.twig', array(
+                        'form'       => $form->createView(),
+                        'signUpForm' => $signUpForm->createView(),
+                    ));  
+                }
+                
+                $rateable = $this->getDoctrine()->getRepository('AcmeRatingBundle:Rateable')->findOneByIdentifier($identifier);
+                if (empty($rateable) === FALSE) {
+                    return $this->redirect($this->generateUrl('rateable_main', array('alphanumericValue' => $alphanumericValue)));
+                }
+
+                $collection = $this->getDoctrine()->getRepository('AcmeRatingBundle:RateableCollection')->findOneByIdentifier($identifier);
+                if (empty($collection) === FALSE) {
+                    return $this->redirect($this->generateUrl('rateable_collection_main', array('alphanumericValue' => $alphanumericValue)));
+                }
+
+                $form->addError(new FormError('Az általad megadott kód nem létezik'));                
+                return $this->render('AcmeRatingBundle:Identifier:index.html.twig', array(
+                    'form'       => $form->createView(),
+                    'signUpForm' => $signUpForm->createView(),
+                ));  
             }
+        }
+    }
+    
+    public function signUpAction(Request $request) {
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('alphanumericValue', 'text', array('attr' => array('placeholder' => $this->get('translator')->trans('Enter Code Here', array(), 'identifier'), 'autocomplete' => 'off')))
+            ->getForm();
+        
+        $signUpForm = $this->createFormBuilder($defaultData, array('csrf_protection' => false))
+            ->add('lastName', 'text', array('attr' => array('autocomplete' => 'off')))
+            ->add('firstName', 'text', array('attr' => array('autocomplete' => 'off')))
+            ->add('email', 'text', array('attr' => array('autocomplete' => 'off')))
+            ->add('company', 'text', array('attr' => array('autocomplete' => 'off')))
+            ->add('message', 'textarea', array('attr' => array('autocomplete' => 'off')))
+            ->getForm();
+        
+        if ( $request->isMethod('POST') ) {
+            $signUpForm->bind($request);                  
+            $email = $signUpForm->get('email')->getData();
+            if(FALSE === Validator::isEmailAddressValid($email)) {                    
+                $signUpForm->addError(new FormError('A megadott email cím nem megfelelő!'));
+                return $this->render('AcmeRatingBundle:Identifier:index.html.twig', array(
+                    'form'       => $form->createView(),
+                    'signUpForm' => $signUpForm->createView(),
+                ));                
+            }
+            $this->get('session')->setFlash('success', $this->get('translator')->trans('Your message was sent!', array(), 'identifier'));
             $this->sendSignUpEmail($signUpForm);
-            return $this->redirect($this->generateUrl('_welcome'));
+            
+            return $this->redirect($this->generateUrl('_welcome'));  
+            
         }
     }
     
